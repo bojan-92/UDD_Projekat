d02f30846db047cc2ed50f0bf78959594b4e23cd
bojan12345 <bojantrifkovic92@gmail.com>
2017-02-13 21:17:08 +0100

    Add dependency injection

--- src/main/java/com/udd/Application.java
+++ src/main/java/com/udd/Application.java
@@ -2,11 +2,14 @@ package com.udd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.udd")
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
 }
--- src/main/java/com/udd/entities/EBook.java
+++ src/main/java/com/udd/entities/EBook.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -38,6 +39,10 @@ public class EBook {
 	@NotNull
 	@Size(max = 100)
 	private String mime;
+	
+	@ManyToOne()
+	private Language bookLanguage;
+	
 	
 	public EBook(){}
 
